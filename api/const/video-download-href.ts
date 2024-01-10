1ab9801da4ce580a2d0ae25850b97cf3bc2f0219
Nikolay <repyah84@gmail.com>
2024-01-10 15:05:19 +0200

    change sever link

--- api/const/video-download-href.ts
+++ api/const/video-download-href.ts
@@ -1,3 +1,3 @@
-export const VIDEO_DOWNLOAD_URL = "https://yootube-load.onrender.com"
+export const VIDEO_DOWNLOAD_URL = "https://video-download-dbps.onrender.com"
 
 // export const VIDEO_DOWNLOAD_URL = "http://localhost:1000"
